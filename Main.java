0e2b24f898d5eb64af8567abc8561a701578782b
vlasJedi <azmodei@protonmail.com>
2022-11-16 00:53:40 +0200

    towers-of-hanoi-solved-but-not-optimal

--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
 import bridgepattern.*;
+import exercises.towershanoi.THanoi;
 
 public class Main {
     public static void main(String[] args) {
-        AbstractDog dog1 = new Barbos();
-        dog1.setDogTraining(new JumpTrainedDog());
-        dog1.bark();
-        AbstractDog dog2 = new Jhonny();
-        dog2.setDogTraining(new SwimmingTrainedDog());
-        dog2.bark();
+//        AbstractDog dog1 = new Barbos();
+//        dog1.setDogTraining(new JumpTrainedDog());
+//        dog1.bark();
+//        AbstractDog dog2 = new Jhonny();
+//        dog2.setDogTraining(new SwimmingTrainedDog());
+//        dog2.bark();
+        THanoi hanoi = new THanoi(4);
+        hanoi.start();
     }
 }
---
+++ exercises/towershanoi/THanoi.java
@@ -0,0 +1,96 @@
+package exercises.towershanoi;
+
+import java.util.*;
+
+public class THanoi {
+    private final List<Bar> bars = new ArrayList<>();
+
+    public THanoi(int numberOfPlates) {
+        for (int i = 0; i < 3; i++) {
+            bars.add(new Bar(i));
+        }
+        for (int i = 0; i < numberOfPlates; i++) {
+            bars.get(0).push(i, null, null);
+        }
+    }
+
+    public void start() {
+        while(bars.get(0).peek() != -1) {
+            bars.get(1).push(bars.get(0).peek(), bars.get(0), bars.get(2));
+            bars.get(0).pop();
+        };
+    }
+
+    private class Bar {
+        private final int id;
+        private final List<Integer> plates = new ArrayList<>();
+
+        public Bar(int id) {
+            this.id = id;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public int peek() {
+            if (plates.size() == 0) {
+                return -1;
+            }
+            return plates.get(plates.size() - 1);
+        }
+
+        public boolean push(int newPlate, Bar pusherBar, Bar otherBar) {
+            if (plates.contains(newPlate)) {
+                System.out.printf("Bar %d already has this plate id %d, error\n", id, newPlate);
+                return false;
+            }
+            int platesNumberToReturn = 0;
+            while(true) {
+                if (peek() < newPlate) {
+                    plates.add(newPlate);
+                    if (pusherBar != null) System.out.printf("From bar %d -> to bar %d accepts new plate so new plates are %s\n", pusherBar.getId(), id, plates);
+                    break;
+                }
+                boolean success = otherBar.push(peek(), this, pusherBar);
+                if (!success) {
+                    System.out.printf("Bar %d failed to push its plate %d\n", id, peek());
+                    return false;
+                }
+                success = pop();
+                System.out.printf("Bar %d -> to bar %d pushed for more space and has %s\n", id, otherBar.getId(), plates);
+                if (!success) {
+                    System.out.printf("Bar %d failed to pop its plate %d\n", id, peek());
+                    return false;
+                }
+                platesNumberToReturn++;
+            }
+            while(platesNumberToReturn != 0) {
+                boolean success = push(otherBar.peek(), otherBar, pusherBar);
+                if (!success) {
+                    System.out.printf("Bar %d failed to return its plate %d\n", id, peek());
+                    return false;
+                }
+                success = otherBar.pop();
+                System.out.printf("Bar %d -> to bar %d has returned own and has %s\n", otherBar.getId(), id, plates);
+                if (!success) {
+                    System.out.printf("Bar %d failed to pop other bar to return its plate %d\n", id, peek());
+                    return false;
+                }
+                platesNumberToReturn--;
+            };
+            if (pusherBar != null) System.out.printf("From bar %d -> to bar %d plates are %s\n", pusherBar.getId(), id, plates);
+            return true;
+        }
+
+        public boolean pop() {
+            if (plates.size() == 0) {
+                System.out.println("Nothing to single pop");
+                return false;
+            }
+            int plate = plates.get(plates.size() - 1);
+            plates.remove(plates.size() - 1);
+            return true;
+        }
+    }
+}
